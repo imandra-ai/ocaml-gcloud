6211318ddb824e6b734bf38d3026a7dc3efc6e71
Dave Aitken <dave.aitken@gmail.com>
2021-10-20 12:40:10 +0100

    wip: walk vs apply

--- src/big_query_util.ml
+++ src/big_query_util.ml
@@ -6,14 +6,15 @@ module Debug = struct
   (** Inlines query params as values for simpler debugging via the BigQuery UI in dev *)
 
   let inline_params ~(params : J.Param.query_parameter list) (t : Q.t) : Q.t =
-    let inline_param =
-      E.Debug.map_param (fun name ->
+    let inline_param e =
+      E.Debug.map_param
+        (fun name ->
           match params |> CCList.find_opt (fun p -> J.Param.name p = name) with
           | None ->
               E.(param name)
           | Some p ->
               J.Param.param_to_expression p )
+        e
     in
-
-    Q.apply_expression inline_param t
+    Q.walk_expression inline_param t
 end
