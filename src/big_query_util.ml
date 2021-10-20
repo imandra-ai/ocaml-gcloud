b5ff7cea8ccecacd07667ce6c62eef0768b3312d
Dave Aitken <dave.aitken@gmail.com>
2021-10-20 11:35:25 +0100

    feat(bq): inline params helper

---
+++ src/big_query_util.ml
@@ -0,0 +1,19 @@
+module J = Big_query.Jobs
+module E = Gcloud_shared.Big_query_dsl.Expression
+module Q = Gcloud_shared.Big_query_dsl.Query_expr
+
+module Debug = struct
+  (** Inlines query params as values for simpler debugging via the BigQuery UI in dev *)
+
+  let inline_params ~(params : J.Param.query_parameter list) (t : Q.t) : Q.t =
+    let inline_param =
+      E.Debug.map_param (fun name ->
+          match params |> CCList.find_opt (fun p -> J.Param.name p = name) with
+          | None ->
+              E.(param name)
+          | Some p ->
+              J.Param.param_to_expression p )
+    in
+
+    Q.apply_expression inline_param t
+end
