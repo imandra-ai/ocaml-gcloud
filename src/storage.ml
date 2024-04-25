b9826b70aa457b450ec77705896ff5772c2b456e
Matt Bray <mattjbray@gmail.com>
2024-04-25 18:58:23 +0100

    feat(storage): insert_object

--- src/storage.ml
+++ src/storage.ml
@@ -1,6 +1,9 @@
 module Scopes = struct
   let devstorage_read_only =
     "https://www.googleapis.com/auth/devstorage.read_only"
+
+  let devstorage_read_write =
+    "https://www.googleapis.com/auth/devstorage.read_write"
 end
 
 let get_object_stream (bucket_name : string) (object_path : string) :
@@ -36,6 +39,42 @@ let get_object (bucket_name : string) (object_path : string) :
   let open Lwt_result.Infix in
   get_object_stream bucket_name object_path >>= fun stream ->
   Lwt_stream.to_list stream |> Lwt.map (String.concat "") |> Lwt_result.ok
+
+let insert_object_ bucket_name name (body : Cohttp_lwt.Body.t) :
+    (unit, [> Error.t ]) Lwt_result.t =
+  let open Lwt_result.Infix in
+  Common.get_access_token ~scopes:[ Scopes.devstorage_read_write ] ()
+  >>= fun token_info ->
+  Lwt.catch
+    (fun () ->
+      let uri =
+        Uri.make () ~scheme:"https" ~host:"storage.googleapis.com"
+          ~path:(Printf.sprintf "upload/storage/v1/b/%s/o" bucket_name)
+          ~query:[ ("name", [ name ]); ("uploadType", [ "media" ]) ]
+      in
+      let headers =
+        Cohttp.Header.of_list
+          [
+            ( "Authorization",
+              Printf.sprintf "Bearer %s" token_info.Auth.token.access_token );
+          ]
+      in
+      Cohttp_lwt_unix.Client.post uri ~headers ~body |> Lwt_result.ok)
+    (fun e -> Lwt_result.fail (`Network_error e))
+  >>= fun (resp, body) ->
+  match Cohttp.Response.status resp with
+  | `OK -> Lwt_result.return ()
+  | status_code -> Error.of_response_status_code_and_body status_code body
+
+let insert_object bucket_name name (data : string) :
+    (unit, [> Error.t ]) Lwt_result.t =
+  let body = Cohttp_lwt.Body.of_string data in
+  insert_object_ bucket_name name body
+
+let insert_object_stream bucket_name name (data : string Lwt_stream.t) :
+    (unit, [> Error.t ]) Lwt_result.t =
+  let body = Cohttp_lwt.Body.of_stream data in
+  insert_object_ bucket_name name body
 
 [@@@warning "-39"]
 
