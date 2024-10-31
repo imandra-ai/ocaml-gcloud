7029c76371624c6c9d4845edb55b22742506048e
Dave Aitken <actionshrimp@users.noreply.github.com>
2024-10-31 10:51:53 +0000

    Cleanup dev setup (#36)
    * chore: nixfmt-rfc-style
    
    * tidy: don't need old nixpkgs for ocamlformat any more
    
    * tidy: nixpkgs rather than dev opam scope for lsp/utop
    
    * bump: nixpkgs version for format check
    
    * refactor(build): onix based env

--- flake.nix
+++ flake.nix
@@ -1,129 +1,40 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-
-    #ocamlformat 0.22.4 is a bit old, so is only in older versions of nixpkgs
-    nixpkgs2305.url = "nixpkgs/nixos-23.05";
-
     flake-utils.url = "github:numtide/flake-utils";
-    opam-nix = {
-      url = "github:tweag/opam-nix";
-      inputs.opam-repository.follows = "opam-repository";
-    };
-    opam-repository = {
-      url = "github:ocaml/opam-repository";
-      flake = false;
-    };
+
+    onix.url = "github:rizo/onix";
+    onix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs2305, flake-utils, opam-nix, opam-repository }@inputs:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          pkgs2305 = nixpkgs2305.legacyPackages.${system};
-          fs = pkgs.lib.fileset;
-          on = opam-nix.lib.${system};
-          ocaml-version = "5.1.0";
-          ocaml-base-compiler = ocaml-version;
+  outputs = { self, nixpkgs, flake-utils, onix }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        onix' = onix.packages.${system}.latest;
+        onixEnv = onix'.env {
+          path = ./.;
+          # roots = [./gcloud.opam ./gcloud-cli.opam];
+          lock = ./onix-lock.json;
+        };
+        onixEnvDev = onix'.env {
+          path = ./.;
+          # roots = [./gcloud.opam ./gcloud-cli.opam];
+          lock = ./onix-lock-dev.json;
+        };
 
-          # List of opam files you which to be read by opam-nix
-          opamFiles = [
-            ./gcloud.opam
-            ./gcloud-cli.opam
+      in rec {
+        # for use by nix fmt
+        formatter = pkgs.nixfmt-rfc-style;
 
-            # Also include the opam files to any submoduled dependencies
-            # ./vendor/some-lib/some-lib.opam
-          ];
+        packages.gcloud-cli = onixEnv.pkgs.gcloud-cli;
 
-          opamFilePackageNames = (map
-            (x:
-              (pkgs.lib.removeSuffix ".opam" (builtins.baseNameOf (toString x))))
-            opamFiles);
+        devShells.onixLock = pkgs.mkShell { buildInputs = [ onix' ]; };
 
-
-          # attrset containing all opam packages defined in the opamFiles list
-          # for a specific OCaml version
-          opamScope = (on.buildOpamProject'
-            {
-              repos = [ opam-repository ];
-              resolveArgs.with-test = true;
-              recursive = true;
-              overlays = on.__overlays ++ [
-                (final: prev:
-                  builtins.foldl'
-                    (acc: pkg:
-                      (acc // {
-                        # Allows us to treat the whole repo as a single unit and `dune build` it together,
-                        # without opam-nix attempting to build each opam package individually.
-                        # Similar to opam install --deps-only
-                        ${pkg} =
-                          prev.${pkg}.overrideAttrs (oa: { dontBuild = true; });
-                      }))
-                    {
-                      # other-package-override = prev.other-package-overide.overrideAttrs (oa: {
-                      #   nativeBuildInputs = oa.nativeBuildInputs ++ (with pkgs; [ makeWrapper git ]);
-                      # });
-                    }
-                    opamFilePackageNames)
-              ];
-            }
-            (fs.toSource {
-              root = ./.;
-              fileset = fs.unions opamFiles;
-            })
-            { inherit ocaml-base-compiler; });
-
-          # build any dev deps on the correct ocaml version, without conflicting with project deps.
-          devOpamScope = (on.queryToScope { repos = [ opam-repository ]; } {
-            inherit ocaml-base-compiler;
-            ocaml-lsp-server = "*";
-            utop = "*";
-          });
-
-          gcloud-cli = pkgs.stdenv.mkDerivation {
-            pname = "gcloud-cli";
-            version = "1.0.0";
-            buildInputs = (map (p: opamScope.${p}) opamFilePackageNames);
-            buildPhase = ''
-              dune build @install -p gcloud,gcloud-cli
-            '';
-
-            installPhase = ''
-              mkdir -p $out/bin
-              cp _build/install/default/bin/* $out/bin/
-            '';
-
-            src = (fs.toSource {
-              root = ./.;
-
-              # Prevent changes to the nix flake file from busting the build cache.
-              # Add any other files which the build should ignore to this list.
-              fileset = fs.difference ./. (fs.unions [
-                ./flake.nix
-                ./flake.lock
-              ]);
-            });
-          };
-
-        in
-
-
-        rec {
-          # for use by nix fmt
-          formatter = pkgs.nixpkgs-fmt;
-
-          packages.gcloud-cli = gcloud-cli;
-
-          packages.default = pkgs.mkShell {
-            buildInputs =
-              (map (p: opamScope.${p}) opamFilePackageNames) ++ [
-                devOpamScope.utop
-                devOpamScope.ocaml-lsp-server
-
-                #ocamlformat 0.22.4 is a bit old, so is only in older versions of nixpkgs
-                pkgs2305.ocamlformat_0_22_4
-              ];
-          };
+        devShells.default = onixEnvDev.shell.overrideAttrs (final: prev: {
+          buildInputs = prev.buildInputs ++ [ pkgs.ocamlformat_0_22_4 onix' ];
         });
+
+        packages.dev-shell = devShells.default.inputDerivation;
+      });
 }
